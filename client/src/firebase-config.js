04add617f71401b7f42344d40cc9061a7479b8f0
AkshatGupta468 <74819556+AkshatGupta468@users.noreply.github.com>
2022-03-28 15:27:11 +0530

    added firebase

---
+++ client/src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOzFk7vhSbJ4f7TxvtSTxMUZ8HfFDRQaI",
+  authDomain: "gcwebathon.firebaseapp.com",
+  projectId: "gcwebathon",
+  storageBucket: "gcwebathon.appspot.com",
+  messagingSenderId: "698463007856",
+  appId: "1:698463007856:web:2fa0a75996d886585c70b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
