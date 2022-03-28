4f913ee9e4d1fe3b0dc63ade6f8c6fea37a42f36
AkshatGupta468 <74819556+AkshatGupta468@users.noreply.github.com>
2022-03-28 23:05:17 +0530

    authentication done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,105 @@
-import './App.css';
-import { firebaseapp } from './firebase-config';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
-import Form from './components/common/Form'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Form from "./components/Common/Form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import { app } from "./firebase-config";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import Home from "./Home";
+import { Button } from "@mui/material";
+toast.configure();
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+        });
+    } else if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email Already in Use");
+          }
+        });
+    }
+  };
+  useEffect(() => {
+    let authToken = sessionStorage.getItem('Auth Token')
+
+    if (authToken) {
+      navigate('/home')
+    }
+  }, []);
   return (
-    <div className="App">
-      <Form/>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <>
+          <Routes>
+            <Route path="*" element={<Navigate to="/home" replace />} />
+            <Route
+              path="/login"
+              element={
+                <Form
+                  title="Login"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction(1)}
+                />
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <Form
+                  title="Register"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction(2)}
+                />
+              }
+            />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </>
+      </div>
+    </React.Fragment>
   );
 }
 
--- client/src/components/common/Form.js
+++ client/src/components/common/Form.js
@@ -1,18 +1,40 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-
-export default function BasicTextFields() {
+import Button from './Button';
+export default function BasicTextFields({ title, setPassword, setEmail, handleAction }) {
     return (
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Box>
+        <div>
+            <div className="heading-container">
+                <h3>
+                    {title} Form
+                </h3>
+            </div>
+
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="email"
+                    label="Enter the Email"
+                    variant="outlined"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    id="password"
+                    label="Enter the Password"
+                    variant="outlined"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </Box>
+
+            <Button title={title} handleAction={handleAction}/>
+        </div>
     );
 }
